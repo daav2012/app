4042501da291e9186cbe79b767ea0d5c81875eb6
daav2012 <daav_2012@hotmail.com>
2022-06-18 19:45:05 -0500

    commit

--- api/routes/emqxapi.js
+++ api/routes/emqxapi.js
@@ -3,13 +3,15 @@ const router = express.Router();
 const axios = require("axios");
 const colors = require("colors");
 
+
+import EmqxAuthRule from "../models/emqx_auth.js";
+
 const auth = {
   auth: {
     username: "admin",
     password: process.env.EMQX_DEFAULT_APPLICATION_SECRET
   }
 };
-
 
 global.saverResource = null;
 global.alarmResource = null;
@@ -34,7 +36,7 @@ Para borrar manualmente los recursos y reiniciemos node */
 async function listResources() {
 
 try {
-    const url = "http://localhost:8085/api/v4/resources/";
+    const url = "http://" + process.env.EMQX_API_HOST +":8085/api/v4/resources/";
 
     const res = await axios.get(url, auth);
   
@@ -98,12 +100,12 @@ try {
 async function createResources() {
 
     try {
-        const url = "http://localhost:8085/api/v4/resources";
+        const url = "http://" + process.env.EMQX_API_HOST +":8085/api/v4/resources";
 
         const data1 = {
             "type": "web_hook",
             "config": {
-                url: "http://localhost:3001/api/saver-webhook",
+                url: "http://" + process.env.WEBHOOKS_HOST +":3001/api/saver-webhook",
                 headers: {
                     token: process.env.EMQX_API_TOKEN
                 },
@@ -115,7 +117,7 @@ async function createResources() {
         const data2 = {
             "type": "web_hook",
             "config": {
-                url: "http://localhost:3001/api/alarm-webhook",
+                url: "http://" + process.env.WEBHOOKS_HOST +":3001/api/alarm-webhook",
                 headers: {
                     token: process.env.EMQX_API_TOKEN
                 },
@@ -150,8 +152,46 @@ async function createResources() {
 }
 
 
+
+//check if superuser exist if not we create one
+global.check_mqtt_superuser = async function checkMqttSuperUser(){
+
+  try {
+    const superusers = await EmqxAuthRule.find({type:"superuser"});
+
+    if (superusers.length > 0 ) {
+  
+      return;
+  
+    }else if ( superusers.length == 0 ) {
+  
+      await EmqxAuthRule.create(
+        {
+          publish: ["#"],
+          subscribe: ["#"],
+          userId: "emqxmqttsuperuser",
+          username: process.env.EMQX_NODE_SUPERUSER_USER,
+          password: process.env.EMQX_NODE_SUPERUSER_PASSWORD,
+          type: "superuser",
+          time: Date.now(),
+          updatedTime: Date.now()
+        }
+      );
+  
+      console.log("Mqtt super user created")
+  
+    }
+  } catch (error) {
+    console.log("error creating mqtt superuser ");
+    console.log(error);
+  }
+}
+
+
+
 setTimeout(() => {
+  console.log("LISTING RESORUCES!!!!!!!!!");
   listResources();
-}, 1000);
+}, process.env.EMQX_RESOURCES_DELAY);
 
 module.exports = router;
